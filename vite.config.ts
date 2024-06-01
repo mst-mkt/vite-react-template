ea18f47d4226d1938a7f72210ab2740a6d3b4e33
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-06-01 23:33:56 +0900

    chore: add config for vite-plugin of tanstack-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
 })
