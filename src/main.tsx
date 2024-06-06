6a9244a9a9efe660599fc365043dde5c47bd392f
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-06-06 11:18:44 +0900

    fix: change to PascalCase

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDom from 'react-dom/client'
 import App from './App.tsx'
 
 // biome-ignore lint/style/noNonNullAssertion: #root is exist in index.html
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDom.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
