f27c1e57896ee36d696a32ea1fe237c783b6413a
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-07-17 11:45:02 +0900

    add: constants of project info

---
+++ src/constants/project.ts
@@ -0,0 +1,3 @@
+export const PROJECT_NAME = 'vite-react-template'
+export const PROJECT_AUTHOR = 'mst-mkt'
+export const PROJECT_REPOSITORY_URL = 'https://github.com/mst-mkt/vite-react-template'
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { PROJECT_NAME } from '../constants/project'
 
 export const Route = createFileRoute('/')({
-  component: () => <div>Hello /!</div>,
+  component: () => <div>Hello {PROJECT_NAME} !</div>,
 })
