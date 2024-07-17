a6166d1cfd26b8486a9494fd7e2161e00e938dd9
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-07-17 13:24:33 +0900

    chore(tanstack-router): add config options for vite-plugin of tanstack-router

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [
     react(),
     TanStackRouterVite({
+      routesDirectory: 'src/routes',
       generatedRouteTree: 'src/lib/tanstack-router/routeTree.gen.ts',
+      routeFileIgnorePrefix: '.',
+      semicolons: false,
+      quoteStyle: 'single',
     }),
   ],
 })
