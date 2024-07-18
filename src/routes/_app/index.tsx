c8a28952b505e62e9d3d25f02d28a86e53e38554
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-07-18 15:14:33 +0900

    refactor: divide route jsx into separate components

--- src/routes/_app/index.tsx
+++ src/routes/_app/index.tsx
@@ -2,5 +2,11 @@ import { createFileRoute } from '@tanstack/react-router'
 import { PROJECT_NAME } from '../../constants/project'
 
 export const Route = createFileRoute('/_app/')({
-  component: () => <div>Hello {PROJECT_NAME} !</div>,
+  component: () => <Home />,
 })
+
+const Home = () => (
+  <div>
+    <p>Hello {PROJECT_NAME} !</p>
+  </div>
+)
