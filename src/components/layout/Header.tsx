462adf5b4feaa281b41f06e91bb1e6f1f84ae9d2
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-07-25 23:29:09 +0900

    fature: toggle theme with click button in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,25 @@
-import { IconBrandGithubFilled } from '@tabler/icons-react'
+import { IconBrightness } from '@tabler/icons-react'
 import { Link } from '@tanstack/react-router'
-import { PROJECT_NAME, PROJECT_REPOSITORY_URL } from '../../constants/project'
+import { PROJECT_NAME } from '../../constants/project'
+import { useTheme } from '../../hooks/useTheme'
 
-export const Header = () => (
-  <header className="sticky top-0 border-b bg-[#fff2] backdrop-blur-md">
-    <div className="mx-auto flex max-w-[600px] items-center gap-y-4 px-6 py-4 font-bold text-2xl">
-      <Link to="/" className="grow transition-colors hover:text-[#4c6]">
-        {PROJECT_NAME}
-      </Link>
-      <a
-        href={PROJECT_REPOSITORY_URL}
-        className="w-fit rounded-md p-2 transition-colors hover:bg-gray-200"
-      >
-        <IconBrandGithubFilled size={20} />
-      </a>
-    </div>
-  </header>
-)
+export const Header = () => {
+  const { toggleTheme } = useTheme()
+
+  return (
+    <header className="sticky top-0 border-b bg-[#fff2] backdrop-blur-md">
+      <div className="mx-auto flex max-w-[600px] items-center gap-y-4 px-6 py-4 font-bold text-2xl">
+        <Link to="/" className="grow transition-colors hover:text-[#4c6]">
+          {PROJECT_NAME}
+        </Link>
+        <button
+          type="button"
+          onClick={toggleTheme}
+          className="w-fit rounded-md p-2 transition-colors hover:bg-gray-200 focus:outline-none"
+        >
+          <IconBrightness size={20} />
+        </button>
+      </div>
+    </header>
+  )
+}
