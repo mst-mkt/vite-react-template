3d17146d18329ab477a2b966c09d3c5fff423aa2
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-07-26 00:19:51 +0900

    fix: update constants for project info

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -1,4 +1,5 @@
 export const PROJECT_ID = 'vite-react-template'
 export const PROJECT_NAME = 'Vite React Template'
-export const PROJECT_AUTHOR = 'mst-mkt'
-export const PROJECT_REPOSITORY_URL = 'https://github.com/mst-mkt/vite-react-template'
+export const PROJECT_AUTHOR = ['mst-mkt'] as const
+export const PROJECT_OWNER = 'mst-mkt'
+export const PROJECT_REPOSITORY_URL = `https://github.com/${PROJECT_OWNER}/${PROJECT_ID}` as const
