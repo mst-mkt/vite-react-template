68e8c007cc78e9aa9489573882eda09137d45ee1
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-07-25 23:58:54 +0900

    add: dynamic head tag with react-helet-async

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
+import { Head } from './components/shared/Head.tsx'
+import { PROJECT_NAME } from './constants/project.ts'
 import './styles/index.css'
 
 // biome-ignore lint/style/noNonNullAssertion: #root is exist in index.html
 ReactDom.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Head title={PROJECT_NAME} />
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 )
--- src/routes/_layout/index.tsx
+++ src/routes/_layout/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Head } from '../../components/shared/Head'
 import { PROJECT_NAME } from '../../constants/project'
 
 export const Route = createFileRoute('/_layout/')({
@@ -7,6 +8,7 @@ export const Route = createFileRoute('/_layout/')({
 
 const Home = () => (
   <div>
+    <Head title="Home" description={`Top Page of ${PROJECT_NAME}`} />
     <p>Hello {PROJECT_NAME} !</p>
   </div>
 )
