4b14ae6d8b6d241d0806196a69405067b62f5a3d
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-04 03:53:12 +0900

    feat(tailwind): add and use theme variables

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,15 +7,15 @@ export const Header = () => {
   const { toggleTheme } = useTheme()
 
   return (
-    <header className="sticky top-0 border-b bg-[#fff2] backdrop-blur-md">
-      <div className="mx-auto flex max-w-[600px] items-center gap-y-4 px-6 py-4 font-bold text-2xl">
-        <Link to="/" className="grow transition-colors hover:text-[#4c6]">
+    <header className="sticky top-0 border-background-200 border-b bg-backgroung/16 backdrop-blur-md">
+      <div className="mx-auto flex max-w-[600px] items-center justify-between gap-y-4 px-6 py-4 font-bold text-2xl">
+        <Link to="/" className="transition-colors hover:text-accent-400">
           {PROJECT_NAME}
         </Link>
         <button
           type="button"
           onClick={toggleTheme}
-          className="w-fit rounded-md p-2 transition-colors hover:bg-gray-200 focus:outline-none"
+          className="w-fit rounded-md p-2 transition-colors hover:bg-background-200 focus:outline-none"
         >
           <IconBrightness size={20} />
         </button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,51 @@ import typography from '@tailwindcss/typography'
 import scrollbar from 'tailwind-scrollbar'
 import type { Config } from 'tailwindcss'
 
+const alphas = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900] as const
+
+const colorMix = (color1: string, color2: string, percentage: number) => {
+  return `color-mix(in srgb, ${color1}, ${color2} ${percentage}%)`
+}
+const hsl = (channels: TemplateStringsArray) => `hsl(var(--${channels[0]}))`
+const hsla = (channels: TemplateStringsArray) => `hsl(var(--${channels[0]}) / <alpha-value>)`
+
+const makeColorPalette = (color: string) => ({
+  50: colorMix(color, 'hsl(var(--foreground))', 70),
+  100: colorMix(color, 'hsl(var(--foreground))', 50),
+  200: colorMix(color, 'hsl(var(--foreground))', 30),
+  300: colorMix(color, 'hsl(var(--foreground))', 10),
+  400: colorMix(color, 'hsl(var(--foreground))', 5),
+  500: color,
+  600: colorMix(color, 'hsl(var(--background))', 10),
+  700: colorMix(color, 'hsl(var(--background))', 30),
+  800: colorMix(color, 'hsl(var(--background))', 50),
+  900: colorMix(color, 'hsl(var(--background))', 70),
+})
+
 const config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          DEFAULT: hsla`background`,
+          ...Object.fromEntries(
+            alphas.map((alpha) => [alpha, colorMix(hsla`background`, hsl`foreground`, alpha / 10)]),
+          ),
+        },
+        foreground: {
+          DEFAULT: hsla`foreground`,
+          ...Object.fromEntries(
+            alphas.map((alpha) => [alpha, colorMix(hsla`foreground`, hsl`background`, alpha / 10)]),
+          ),
+        },
+        accent: {
+          DEFAULT: hsla`accent`,
+          ...makeColorPalette(hsla`accent`),
+        },
+      },
+    },
   },
   plugins: [
     scrollbar({ nocompatible: true, preferredStrategy: 'pseudoelements' }),
