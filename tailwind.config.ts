a827693b0f3dce7a4346eb3138241f62a863e408
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-10-15 21:42:48 +0900

    chore(tailwind): update config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,15 @@ const alphas = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900] as const
 const colorMix = (color1: string, color2: string, percentage: number) => {
   return `color-mix(in srgb, ${color1}, ${color2} ${percentage}%)`
 }
-const oklch = (channels: TemplateStringsArray) => `oklch(var(--${channels[0]}))`
-const oklchAlpha = (channels: TemplateStringsArray) => `oklch(var(--${channels[0]}) / <alpha-value>)`
+
+const oklch = ([color]: TemplateStringsArray) => `oklch(var(--${color}) / <alpha-value>)`
 
 const makeColorPalette = (color: string) => ({
-  50: colorMix(color, oklch`foreground`, 70),
-  100: colorMix(color, oklch`foreground`, 50),
-  200: colorMix(color, oklch`foreground`, 30),
-  300: colorMix(color, oklch`foreground`, 10),
-  400: colorMix(color, oklch`foreground`, 5),
+  50: colorMix(color, oklch`background`, 70),
+  100: colorMix(color, oklch`background`, 50),
+  200: colorMix(color, oklch`background`, 30),
+  300: colorMix(color, oklch`background`, 10),
+  400: colorMix(color, oklch`background`, 5),
   500: color,
   600: colorMix(color, oklch`foreground`, 10),
   700: colorMix(color, oklch`foreground`, 30),
@@ -33,20 +33,20 @@ const config = {
       },
       colors: {
         background: {
-          DEFAULT: oklchAlpha`background`,
+          DEFAULT: oklch`background`,
           ...Object.fromEntries(
-            alphas.map((alpha) => [alpha, colorMix(oklchAlpha`background`, oklch`foreground`, alpha / 10)]),
+            alphas.map((alpha) => [alpha, colorMix(oklch`background`, oklch`foreground`, alpha / 10)]),
           ),
         },
         foreground: {
-          DEFAULT: oklchAlpha`foreground`,
+          DEFAULT: oklch`foreground`,
           ...Object.fromEntries(
-            alphas.map((alpha) => [alpha, colorMix(oklchAlpha`foreground`, oklch`background`, alpha / 10)]),
+            alphas.map((alpha) => [alpha, colorMix(oklch`foreground`, oklch`background`, alpha / 10)]),
           ),
         },
         accent: {
-          DEFAULT: oklchAlpha`accent`,
-          ...makeColorPalette(oklchAlpha`accent`),
+          DEFAULT: oklch`accent`,
+          ...makeColorPalette(oklch`accent`),
         },
       },
     },
