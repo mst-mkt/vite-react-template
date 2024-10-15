5bfa6d339867d51b7122c762d47d188c181edd9d
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-10-15 19:45:55 +0900

    chore(deps): remove `zod`

--- src/routes/_layout/index.tsx
+++ src/routes/_layout/index.tsx
@@ -1,27 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { z } from 'zod'
 import { Head } from '../../components/shared/Head'
 import { PROJECT_NAME } from '../../constants/project'
 
-const validationSearchParams = z.object({
-  params: z.union([z.string().transform((value) => [value]), z.array(z.string())]).optional(),
-})
+// const validationSearchParams = z.object({
+//   params: z.union([z.string().transform((value) => [value]), z.array(z.string())]).optional(),
+// })
 
 export const Route = createFileRoute('/_layout/')({
-  validateSearch: (search) => validationSearchParams.parse(search),
+  // validateSearch: (search) => validationSearchParams.parse(search),
   component: () => <Home />,
 })
 
 const Home = () => {
-  const { params } = Route.useSearch()
+  // const { params } = Route.useSearch()
 
   return (
     <div>
       <Head title="Home" description={`Top Page of ${PROJECT_NAME}`} />
       <p>Hello {PROJECT_NAME} !</p>
-      {params !== undefined && params.length > 0 && (
+      {/* {params !== undefined && params.length > 0 && (
         <p>Params: {params.map((param) => param).join(', ')}</p>
-      )}
+      )} */}
     </div>
   )
 }
