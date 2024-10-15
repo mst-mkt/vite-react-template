08a934bdcce8e9196354993f3b5c94f130b19b30
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-10-15 10:30:01 +0900

    refactor: useTheme without `atomWithStorage`

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,28 +1,34 @@
-import { useAtom } from 'jotai'
-import { atomWithStorage } from 'jotai/utils'
 import { useCallback, useEffect } from 'react'
 import { THEMES } from '../constants/theme'
+import { useLocalStorage } from './useLocalStorage'
 
 type Theme = (typeof THEMES)[number]
-
 const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
 
-const themeAtom = atomWithStorage<Theme>('theme', isDarkTheme ? 'dark' : 'light')
+export const setThemeWithoutRender = () => {
+  const theme = localStorage.getItem('theme')
+  const isLightTheme = theme === 'light' || (theme === null && !isDarkTheme)
+  document.body.classList.add(isLightTheme ? 'light' : 'dark')
+}
 
 export const useTheme = () => {
-  const [theme, setTheme] = useAtom(themeAtom)
+  const [theme, setTheme] = useLocalStorage<Theme>('theme', isDarkTheme ? 'dark' : 'light')
+
+  const setThemeClassNames = useCallback((theme?: Theme) => {
+    document.body.classList.remove(...THEMES)
+    if (theme !== undefined) {
+      document.body.classList.add(theme)
+    }
+  }, [])
+
+  const toggleTheme = useCallback(() => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }, [setTheme, theme])
 
   useEffect(() => {
-    document.body.classList.remove(...THEMES)
-    document.body.classList.add(theme)
-  }, [theme])
+    setThemeClassNames(theme)
+    return () => setThemeClassNames()
+  }, [theme, setThemeClassNames])
 
-  return {
-    theme,
-    setTheme: useCallback((theme: Theme) => setTheme(theme), [setTheme]),
-    toggleTheme: useCallback(
-      () => setTheme((prev) => (prev === 'light' ? 'dark' : 'light')),
-      [setTheme],
-    ),
-  }
+  return { theme, setTheme, toggleTheme }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import { HelmetProvider } from 'react-helmet-async'
 import { App } from './App.tsx'
 import { Head } from './components/shared/Head.tsx'
 import { PROJECT_NAME } from './constants/project.ts'
+import { setThemeWithoutRender } from './hooks/useTheme.ts'
 import './styles/index.css'
 
 const rootDom = document.getElementById('root')
+if (rootDom === null) throw new Error('root element not found')
 
-if (rootDom === null) {
-  throw new Error('Root element not found')
-}
+setThemeWithoutRender()
 
 ReactDom.createRoot(rootDom).render(
   <React.StrictMode>
